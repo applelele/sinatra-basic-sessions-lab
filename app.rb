9a041f89b63f93f0d58b549fd29d33ec39729c26
applelele <applelele@gmail.com>
2021-08-14 18:01:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions # turns sessions on
+        set :session_secret, 'secret' # an encryption key that  will be used to create a session_id.
+    end
+
+    get '/' do
+        erb :index # display the form to enter an item
+    end
+
+    post '/checkout' do
+        @item = params[:item] # set an instance variable to assign param data from the form (index)
+        session[:item] = @item # add the param data from the form to the session hash
+        @session = session # store the session hash in an instance variable that you can access in the views
+
+        erb :checkout
+    end
 end
